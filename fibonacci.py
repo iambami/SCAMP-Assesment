1c06164451f5cf06498475f511d7fe0bffc722f4
iambami <64528871+iambami@users.noreply.github.com>
2020-05-01 13:34:16 +0100

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+﻿i = 0
+Value_One = 0
+Value_Two = 1
+           
+# Find & Displaying Fibonacci series
+while(i < Number):
+           if(i <= 1):
+                      Next = i
+           else:
+                      Next = Value_One + Value_Two
+                      Value_One = Value_Two
+                      Value_Two = Next
+           print(Next)
+           i = i + 1
